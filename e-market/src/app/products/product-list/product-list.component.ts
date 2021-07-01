72eead26e28a0c45ea6d681568bacddd94532ca8
MohammadRAmjad <mohammad.amjad@revature.net>
2021-07-01 09:34:15 -0700

    category working

--- e-market/src/app/products/product-list/product-list.component.ts
+++ e-market/src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
 import {Product} from '../../shared/product.model';
 import { ProductService } from '../product.service';
 
@@ -8,15 +9,22 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
+ 
   
   @Output() productWasSelected = new EventEmitter<Product>()
+  @Input() categorySelected = new EventEmitter<string>()
   products: Product[] = [];
+  value:string="All";
+  
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.products =this.productService.getProducts()
+    this.products =this.productService.getProducts(this.value)
+  }
+
+  buttonClick(e:string){
+    this.products =this.productService.getProducts(e)
   }
 
   onProductSelect(product: Product){
--- e-market/src/app/products/product.service.ts
+++ e-market/src/app/products/product.service.ts
@@ -4,9 +4,25 @@ export class ProductService {
 
 private products: Product[] = [
     new Product(1,'TV', 750.55, 'Electronics'),
-    new Product(2,'TV', 755.55, 'Electronics')
+    new Product(2,'PS5', 499.00, 'Electronics'),
+    new Product(3,'Java', 30.00, 'Books'),
+    new Product(4,'Jacket', 100.00, 'Clothing'),
+    new Product(5,'Table', 60.00, 'Furniture')
   ];
-  getProducts(){
-      return this.products.slice();
+  getProducts(v:string){
+      if(v === 'All')
+        return this.products.slice();
+      else{
+        let selectedProducts: Product[] = [];
+        for (var item of this.products){
+            if(item.category === v)
+                selectedProducts.push(item)
+                console.log(item.title)
+        }
+        console.log(selectedProducts.length)
+        return selectedProducts
+      }
+      
+        
   }
 }
